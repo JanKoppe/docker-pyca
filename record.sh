4c3827f9d0c46b8566d21255d654e9a39615385a
Jan Koppe <post@jankoppe.de>
2016-08-23 15:37:10 +0200

    reduce log spam, reduce baselevel for output files

--- record.sh
+++ record.sh
@@ -6,13 +6,23 @@ NAME=$3
 
 pids=""
 
-ffmpeg -nostats -re -f lavfi -i testsrc -t $TIME -pix_fmt yuv420p -s 4096x2160 $DIR/$NAME-4k.mp4 &
+ffmpeg -loglevel fatal -y  -re -f lavfi -i testsrc -t $TIME -pix_fmt yuv420p -s 4096x2160 \
+  -an \
+  -c\:v libx264 \
+  -profile\:v baseline -level 3.1 \
+  -preset faster \
+  $DIR/$NAME-4k.mp4 &
 pids+=" $!"
 
-ffmpeg -nostats -re -f lavfi -i testsrc -t $TIME -pix_fmt yuv420p -s 1280x720 $DIR/$NAME-720.mp4 &
+ffmpeg -loglevel fatal -y  -re -f lavfi -i testsrc -t $TIME -pix_fmt yuv420p -s 1280x720 \
+  -an \
+  -c\:v libx264 \
+  -profile\:v baseline -level 3.1 \
+  -preset faster \
+   $DIR/$NAME-720.mp4 &
 pids+=" $!"
 
-ffmpeg -f lavfi -i "sine=frequency=440:duration=$TIME" $DIR/$NAME-audio.mp3 &
+ffmpeg -loglevel fatal -y  -f lavfi -i "sine=frequency=440:duration=$TIME" $DIR/$NAME-audio.mp3 &
 pids+=" $!"
 
 #   Wait for recordings to finish (running as background processes), then finish
